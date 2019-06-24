b74b8f1e419bc1abb3664914e8b54b4aa10f5b44
Feng MengJi <dream_0926@163.com>
2019-06-24 18:08:01 +0800

    添加图片像素比例缩小

--- src/main/java/com/luckyxs/conversion/ParseImg.java
+++ src/main/java/com/luckyxs/conversion/ParseImg.java
@@ -3,10 +3,9 @@ package com.luckyxs.conversion;
 import com.luckyxs.conversion.vo.ImgVo;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -118,5 +117,45 @@ public class ParseImg {
             e.printStackTrace();
         }
 
+    }
+
+    /**
+     *  图片减少像素
+     * @param file 图片文件
+     * @param path 新生成的图片地址
+     * @return
+     * @throws IOException
+     */
+    public static File reducePicturePixel(File file,String path) throws IOException {
+        //读取图片
+        BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
+        //字节流转图片对象
+        Image bi =ImageIO.read(in);
+        //获取图像的高度，宽度
+        int height=bi.getHeight(null);
+        int width =bi.getWidth(null);
+        // 长宽比例
+        double hw = height/(width*1.0);
+        //判断像素长宽是否高于500
+        for (;;){
+            if (width>200)
+                width = width - 100;
+            else{
+                height =(int)(width * hw);
+                break;
+            }
+        }
+        //构建图片流
+        BufferedImage tag = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        //绘制改变尺寸后的图
+        tag.getGraphics().drawImage(bi, 0, 0,width, height, null);
+        //输出流
+        BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(path));
+        //JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
+        //encoder.encode(tag);
+        ImageIO.write(tag, "PNG",out);
+        in.close();
+        out.close();
+        return new File(path);
     }
 }
--- src/main/java/com/luckyxs/main/Main.java
+++ src/main/java/com/luckyxs/main/Main.java
@@ -4,7 +4,10 @@ import com.luckyxs.conversion.ParseImg;
 import com.luckyxs.conversion.vo.ImgVo;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
+import static com.luckyxs.conversion.ParseImg.reducePicturePixel;
 
 /**
  * 程序主入口
@@ -16,10 +19,11 @@ public class Main {
      * 主程序入口
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         long startTime = System.currentTimeMillis();    //获取开始时间
         // 需要上传的图片地址
-        File f = new File("C:\\Users\\fengm\\Desktop\\1.png");
+        File f = new File("D:\\java\\WorkSpace\\ideaSpace\\parse_picture\\src\\main\\java\\com\\luckyxs\\conversion\\1.jpg");
+        f = reducePicturePixel(f,"D:\\java\\WorkSpace\\ideaSpace\\parse_picture\\src\\main\\java\\com\\luckyxs\\conversion\\copy.jpg");
         // 获取图片的RGB解析集合
         List<ImgVo> list = ParseImg.getImgRGB(f);
         // 最好使用文件的绝对路径
